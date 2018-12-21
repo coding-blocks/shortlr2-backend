9764464a053d192b15ff64bdb177a1e912c5fa62
Jatin Katyal <jatin.katyal13@gmail.com>
2018-12-21 16:27:01 +0530

    update and create api

--- src/routes/api/urls.ts
+++ src/routes/api/urls.ts
@@ -4,10 +4,13 @@ import querystring from 'querystring'
 import Raven from 'raven'
 import { findGroupById, findGroupByPrefix } from '../../controllers/groups'
 import {
+  createUrl,
   findUrlByCodeInt,
   findUrlByShortcode,
   getAllUrlsForUser,
   PageOptions,
+  updateUrl,
+  URLOptions,
 } from '../../controllers/urls'
 import paginationMiddleware from '../../middlewares/pagination'
 import { optsFromGroupedShortcode } from '../../utils/shortener'
@@ -57,11 +60,46 @@ route.get('/', async (req, res) => {
   res.send(JSON.stringify(urlsAndPagination))
 })
 
+route.post('/', async (req, res) => {
+  try {
+    const url = await createUrl(
+      {
+        longUrl: req.body.longUrl,
+        shortCode: req.body.shortCode,
+        private: req.body.private,
+      },
+      req.user,
+    )
+    if (!url) {
+      throw new Error('Error creating shortlink. Try again')
+    }
+
+    res.send(url)
+  } catch (e) {
+    Raven.captureException(e)
+    res.send(e)
+  }
+})
+
 route.get('/:url', async (req, res) => {
   try {
     const url = await findUrlByShortcode(req.params.url)
 
     res.send(JSON.stringify(url))
+  } catch (e) {
+    Raven.captureException(e)
+    res.send(e)
+  }
+})
+
+route.post('/:url', async (req, res) => {
+  try {
+    const newUrl: URLOptions = {
+      longUrl: req.body.longUrl,
+      private: req.body.private,
+    }
+    const urlOpts = await updateUrl(req.params.url, newUrl, req.user)
+    res.send(JSON.stringify(urlOpts))
   } catch (e) {
     Raven.captureException(e)
     res.send(e)
@@ -81,6 +119,25 @@ route.get('/:group/:url', async (req, res) => {
     }
 
     res.send(JSON.stringify(url))
+  } catch (e) {
+    Raven.captureException(e)
+    res.send(e)
+  }
+})
+
+route.post('/:group/:url', async (req, res) => {
+  try {
+    const newUrl: URLOptions = {
+      longUrl: req.body.longUrl,
+      private: req.body.private,
+    }
+    const group = await findGroupByPrefix(req.params.group)
+    if (!group) {
+      throw new Error('Group prefic does not exist')
+    }
+    const urlOpts = await updateUrl(req.params.url, newUrl, req.user, group)
+
+    res.send(JSON.stringify(urlOpts))
   } catch (e) {
     Raven.captureException(e)
     res.send(e)
