5a024f03db46ddfb00f64d9a7cf645e7652cee85
Muhammad Farhan <farhanshah.96930012@gmail.com>
2019-05-21 23:04:15 +0530

    authTokens implemented

--- src/passport/setup.ts
+++ src/passport/setup.ts
@@ -1,11 +1,11 @@
 import debug from 'debug'
 import passport from 'passport'
+import AuthTokenStrategy from 'passport-auth-token'
 import { OneauthProfile, Strategy as OneauthStrategy } from 'passport-oneauth'
-import * as path from 'path'
 import Raven from 'raven'
 import config = require('../../config.js')
 import { findCreateFindUser, findUserById } from '../controllers/users'
-import { UserAttributes, UserRole, Users } from '../db'
+import { AuthTokens, UserAttributes, UserRole, Users } from '../db'
 
 const log = debug('shortlr:auth:oneauth')
 
@@ -23,6 +23,27 @@ passport.deserializeUser((userId: number, done) => {
       done(err)
     })
 })
+
+passport.use(
+  'authtoken',
+  new AuthTokenStrategy(async (token, done) => {
+    try {
+      const authToken = await AuthTokens.findOne({
+        where: {
+          token,
+        },
+      })
+      if (authToken) {
+        const user = await Users.findById(authToken.userId)
+        return done(null, user)
+      }
+      done(null, false)
+    } catch (err) {
+      Raven.captureException(err)
+      done(err)
+    }
+  }),
+)
 
 passport.use(
   new OneauthStrategy(
--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,3 +1,10 @@
 import { Router } from 'express'
+import passport from 'passport'
 
 export const route = Router()
+
+route.use(
+  passport.authenticate('authtoken', {
+    session: false,
+  })
+)
